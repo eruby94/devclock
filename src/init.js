443e087165f3bb150de8d40ab0979da245ea6c71
Evan Ruby <evanruby94@gmail.com>
2018-03-21 16:06:30 -0400

    Handle init script (first attempt)

--- src/init.js
+++ src/init.js
@@ -1,4 +1,31 @@
+import fs from 'fs'
+import inquirer from 'inquirer'
+
 export default () => {
-  // TODO create a devclock.config.js in project directory
-  console.log('Initializing config...');
-};
+  const prompts = []
+  // If no config is found
+  if (!fs.existsSync(`${process.cwd()}/devclock.config.json`)) {
+    // Add devclock sheets to the .gitignore
+    fs.appendFileSync(`${process.cwd()}/.gitignore`, '\n.*.devclock\n')
+    // Add prompt for the project name
+    prompts.push({
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of this project? (e.g. My Project)',
+      default: 'My Project',
+    })
+    inquirer.prompt(prompts).then((answers) => {
+      // Write initial config to devclock.config.json
+      fs.writeFile(
+        `${process.cwd()}/devclock.config.json`,
+        JSON.stringify({ title: answers.title }, null, 2),
+        (err) => {
+          if (err) throw err
+          console.log('devclock successfully configured!')
+        },
+      )
+    })
+  } else {
+    console.log('devclock config file exists, aborting...')
+  }
+}
