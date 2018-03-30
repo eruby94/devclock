f5a8c90dd1a5ef1a98597caf9ece3286c0610a01
Evan Ruby <evanruby94@gmail.com>
2018-03-30 16:54:10 -0400

    Refactor for more elegant async code

--- src/out.js
+++ src/out.js
@@ -2,15 +2,14 @@ import series from 'async/series'
 import { checkForUser, extractUser, adjustTimesheet } from './util'
 
 export default () => {
-  const fnSeries = []
-  fnSeries.push((cb) => {
-    checkForUser(cb)
-  })
-  fnSeries.push((cb) => {
-    extractUser(cb)
-  })
-  series(fnSeries, (err, results) => {
-    if (err) throw err
-    adjustTimesheet(results[results.length - 1], false)
-  })
+  series(
+    {
+      checkForUser,
+      user: extractUser,
+    },
+    (err, results) => {
+      if (err) throw err
+      adjustTimesheet(results.user, false)
+    },
+  )
 }
--- src/setup.js
+++ src/setup.js
@@ -1,34 +1,50 @@
 import fs from 'fs'
 import os from 'os'
 import inquirer from 'inquirer'
+import series from 'async/series'
 import { rootConfigExists } from './util'
 
-export default () => {
-  // Check for root configuration folder
-  if (!rootConfigExists()) {
-    // If not found, prompt for user info
-    fs.mkdir(`${os.homedir()}/.devclock`, (err) => {
+const createRootConfigDir = (cb) => {
+  fs.mkdir(`${os.homedir()}/.devclock`, (err) => {
+    if (err) throw err
+    cb()
+  })
+}
+
+const getDeveloperInfo = (cb) => {
+  const prompts = [
+    {
+      type: 'input',
+      name: 'fullName',
+      message: 'What is your full name?',
+    },
+  ]
+  inquirer.prompt(prompts).then((answers) => {
+    fs.writeFile(`${os.homedir()}/.devclock/dev.json`, JSON.stringify(answers, null, 2), (err) => {
       if (err) throw err
-      const prompts = [
-        {
-          type: 'input',
-          name: 'fullName',
-          message: 'What is your full name?',
-        },
-      ]
-      inquirer.prompt(prompts).then((answers) => {
-        fs.writeFile(
-          `${os.homedir()}/.devclock/dev.json`,
-          JSON.stringify(answers, null, 2),
-          (writeErr) => {
-            if (writeErr) throw writeErr
-            console.log('Devclock profile setup complete :)')
-          },
-        )
-      })
+      cb()
+    })
+  })
+}
+
+const createProjectList = (cb) => {
+  fs.writeFile(
+    `${os.homedir()}/.devclock/projects.json`,
+    JSON.stringify({ projectList: [] }),
+    (err) => {
+      if (err) throw err
+      cb()
+    },
+  )
+}
+
+export default () => {
+  if (!rootConfigExists()) {
+    series([createRootConfigDir, getDeveloperInfo, createProjectList], (err) => {
+      if (err) throw err
+      console.log('Devclock profile setup complete :)')
     })
   } else {
-    // Otherwise, provide log message
     console.log('Profile exists, skipping setup')
   }
 }
